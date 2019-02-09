fadbea955385d92576bab10cbf1ec5ae0cc195f0
Mayank_MP5 <mayank5pande@gmail.com>
2019-02-09 12:52:23 +0530

    Messed up With Files Redefining folder Structure

--- css/responsive/responsive_js.js
+++ css/responsive/responsive_js.js
@@ -1,6 +1,18 @@
-
+function showbar(){
+    
 var width = window.innerWidth;
-
-if(width < 400){
-    document.getElementsByClassName("fixedleftbar").style.display = 'none';
+var x = document.getElementById("leftbar");
+ 
+if(width < 410){
+        document.getElementById('home1').href = '';
+        document.getElementById('home2').href = '';
+        document.getElementById('manually').href = '';
+        document.getElementById('csslink').href = "css/responsive/mobile.css"
+}else{
+        document.getElementById('home1').href = '';
+        document.getElementById('home1').href = '';
+        document.getElementById('home2').href = '';
+        document.getElementById('manually').href = '';
+        document.getElementById('csslink').href = "css/responsive/mobile.css";
+}
 }
